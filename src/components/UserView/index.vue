e4589d30ec5e9475637efc1180b3c9e21cbe08cd
KJlmfe <kjlmfe@gmail.com>
2018-03-10 23:53:43 +0800

    feat(userview): add UserView and set route user/:address

---
+++ src/components/UserView/index.vue
@@ -0,0 +1,118 @@
+<template>
+  <main class="UserView">
+    <div class="columns box">
+      <div class="column is-one-quarter">
+        <figure class="user-avatar image is-128x128">
+          <img src="../../assets/avatar.png">
+        </figure>
+      </div>
+      <div class="column">
+        <ul class="user-infoList">
+          <li v-for="key in ['address','balance','income']"
+              :key="key">
+            <span class="info-label">{{$t('UserView.'+key)}}:</span>
+            <span class="info-content">{{user[key]}}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <section>
+      <b-tabs v-model="activeTab">
+        <b-tab-item :label="$t('UserView.tabs.playedDApps')">
+          <b-table class=""
+                   :data="playedDApps"
+                   default-sort-direction="asc"
+                   default-sort="income"
+                   :mobile-cards="false">
+            <template slot-scope="props">
+              <b-table-column :label="$t('UserView.playedDApps.index')"
+                              width="80">
+                {{ props.index+1 }}
+              </b-table-column>
+              <b-table-column field="name"
+                              :label="$t('UserView.playedDApps.name')">
+                <router-link class="navbar-item"
+                             :to="{ name: 'ItemView', params:{ id:props.row.id}}">
+                  {{ props.row.name }}</router-link>
+              </b-table-column>
+              <b-table-column field="income"
+                              :label="$t('UserView.playedDApps.income')"
+                              sortable>
+                {{ props.row.income }}
+              </b-table-column>
+            </template>
+          </b-table>
+        </b-tab-item>
+
+        <b-tab-item :label="$t('UserView.tabs.messageBoard')">
+          <Disqus :shortname="$config.disqus.shortname"
+                  :identifier="'user-'+address"></Disqus>
+        </b-tab-item>
+      </b-tabs>
+    </section>
+  </main>
+</template>
+
+<script>
+import Disqus from 'vue-disqus/VueDisqus';
+import { getUser } from '@/api';
+
+export default {
+  name: 'UserView',
+
+  components: {
+    Disqus,
+  },
+
+  data() {
+    return {
+      activeTab: 0,
+      user: {},
+    };
+  },
+
+  computed: {
+    address() {
+      return this.$route.params.address.toUpperCase();
+    },
+    playedDApps() {
+      return this.user ? this.user.playedDApps : [];
+    },
+  },
+
+  async created() {
+    this.user = await getUser(this.address);
+  },
+
+  methods: {},
+
+  watch: {},
+};
+</script>
+
+<style scoped>
+.UserView {
+  margin-top: 2rem;
+}
+.user-avatar {
+  margin: auto;
+}
+.user-infoList {
+  margin-top: 10px;
+  list-style-type: none;
+}
+.info-label {
+  font-size: 15px;
+  font-weight: 600;
+  line-height: 36px;
+  color: #444;
+}
+.info-content {
+  color: #1a1a1a;
+  font-size: 15px;
+  overflow-wrap: break-word;
+  word-wrap: break-word;
+  word-break: break-all;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import HomeView from '@/views/HomeView';
 import ItemView from '@/views/ItemView';
+import UserView from '@/components/UserView/index';
 import NotFoundView from '@/views/NotFoundView';
 
 Vue.use(Router);
@@ -17,6 +18,11 @@ export default new Router({
       name: 'ItemView',
       path: '/item/:id(\\d+)',
       component: ItemView,
+    },
+    {
+      name: 'UserView',
+      path: '/user/:address',
+      component: UserView,
     },
     {
       name: 'NotFoundView',
