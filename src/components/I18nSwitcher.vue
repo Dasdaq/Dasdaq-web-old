31d0744705f4ad2e86b3d870082a8f7b07ead6a8
KJlmfe <kjlmfe@gmail.com>
2018-03-10 14:25:48 +0800

    feat(i18n): add I18nSwitcher component

---
+++ src/components/I18nSwitcher.vue
@@ -0,0 +1,36 @@
+<template>
+  <select class="I18nSwitcher"
+          v-model="locale">
+    <option v-for="(item) in $config.i18n"
+            :key="item.locale"
+            :value="item.locale">
+      {{item.langDisplay}}</option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'I18nSwitcher',
+
+  computed: {
+    locale: {
+      get() {
+        return this.$store.state.locale;
+      },
+      set(value) {
+        this.$store.dispatch('setLocale', value);
+      },
+    },
+  },
+
+  created() {
+    this.$store.dispatch('initLocale');
+  },
+
+  watch: {
+    locale(val) {
+      this.$i18n.locale = val;
+    },
+  },
+};
+</script>
