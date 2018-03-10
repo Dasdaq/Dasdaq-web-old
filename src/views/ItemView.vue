375f102e589ec7cf20cf403a79471a55672a9379
louie <louieliu0703@gmail.com>
2018-03-10 20:23:58 +0800

    feat(itemview): change itemview and add some icon about itemview

--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -1,8 +1,36 @@
 <template>
   <main class="ItemView">
-    ItemView {{id}}
 
-    <section>
+     <div class="middle">
+    <div class="content">
+      <div class="columns is-desktop">
+        <div class="column is-three-quarters-desktop is-two-thirds-tablet">
+          <h1>ForkDelta</h1><span class="category">游戏</span>
+        </div>
+        <div class="column">
+          <a class="button is-primary is-inverted is-outlined">平台官网</a>
+        </div>
+      </div>
+      <p>
+        <span class="icon item"><img style="background: #4fd2b3;border-radius:50%;" src="../../static/images/header/time.png" alt="time" /></span>
+        上线时间：<span>2017-8-29</span>
+      </p>
+      <div class="">
+        <span class="icon item1 item"><img style="background: #4fd2b3;border-radius:50%;" src="../../static/images/header/tel.png" alt="tel" /></span>
+        联系方式：
+        <label class="">
+          <a href=""><span class="icon"><img src="../../static/images/header/twitter.png" alt="twitter" /></span></a>
+          <a href=""><span class="icon"><img src="../../static/images/header/Reddit.png" alt="reddit" /></span></a>
+          <a href=""><span class="icon"><img src="../../static/images/header/Telegram.png" alt="telegram" /></span></a>
+          <a href=""><span class="icon"><img src="../../static/images/header/WeChat.png" alt="wechat" /></span></a>
+          <a href=""><span class="icon"><img src="../../static/images/header/weibo.png" alt="weibo" /></span></a>
+        </label>
+      </div>
+      
+    </div>
+    
+  </div>
+  <section>
       <b-tabs v-model="activeTab">
         <b-tab-item :label="$t('itemView.tabs.transaction')">
           <TransactionStatistics :itemId="id" />
@@ -22,8 +50,8 @@
         </b-tab-item>
       </b-tabs>
     </section>
-
   </main>
+  
 </template>
 
 <script>
@@ -55,7 +83,7 @@ export default {
   },
 
   created() {},
-
+  
   methods: {},
 
   watch: {},
@@ -63,5 +91,68 @@ export default {
 </script>
 
 <style scoped>
+  .middle{
+    background:#4fd2b3;
+  }
+  .content{
+    background:#4fd2b3;
+    padding:6% 0 1% 12%;
+    color:#fff;
+    font-size:.8em;
+    margin-bottom: 0; 
+  }
+  h1{
+    color:#fff;
+    font-weight: 700;
+    font-size:3em;
+    display: inline;
+  }
+  label{
+    display: inline-block;
+    vertical-align: middle;
+  }
+  a{
+     text-decoration:none;
+     outline: none;
+     color:#fff;
+     cursor: pointer;
+  }
 
+  img{
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    background: #fff;
+  }
+  .category{
+    display: inline-block;
+    font-size:.5em;
+    vertical-align: top;
+    margin-left:1.2em;
+    padding:0 .8em;
+    background:#fff;
+    color:#4fd2b3; 
+    
+  }
+  .icon{
+    margin:0 .1em;
+  }
+  .item{
+    vertical-align: bottom;
+    margin-right: .2em;
+  }
+  .item1{
+    vertical-align: top;
+  }
+  .tabs ul li{
+    color:#fff;
+    background: #4fd2b3;
+  }
+  .tabs {
+    padding:.1em 12%;
+  }
+  .tabs li.is-active a{
+    color:#fff;
+    border-bottom-color:#000;
+  }
 </style>
