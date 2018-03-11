f254dcec56d6b20701e986269b636f5912d12cf7
KJlmfe <kjlmfe@gmail.com>
2018-03-11 09:05:47 +0800

    fix(web3): use infura as web3 http provider if no Metamask

--- src/config/index.js
+++ src/config/index.js
@@ -15,3 +15,29 @@ export const disqus = {
 
 export const apiHost = 'http://10.30.4.57:5000';
 export const submitDappLink = 'https://baidu.com';
+
+export const network = {
+  1: {
+    default: true, // It will be used when no metaMask
+    name: 'Main Ethereum Network',
+    rpc: 'https://mainnet.infura.io/',
+  },
+  2: {
+    name: 'Morden Test Network',
+  },
+  3: {
+    name: 'Ropsten Test Network',
+    rpc: 'https://ropsten.infura.io/',
+  },
+  4: {
+    name: 'Rinkeby Test Network',
+    rpc: 'https://rinkeby.infura.io/',
+  },
+  42: {
+    name: 'Kovan Test Network',
+    rpc: 'https://kovan.infura.io/',
+  },
+};
+
+export const defaultNetwork = Object.values(network).find(net => net.default);
+
--- src/web3.js
+++ src/web3.js
@@ -1,5 +1,9 @@
 import Web3 from 'web3';
+import { defaultNetwork } from '@/config';
 
-const web3 = window.web3 ? new Web3(window.web3.currentProvider) : null;
+const web3Provider = window.web3 ? window.web3.currentProvider : null;
+const web3 = web3Provider
+  ? new Web3(web3Provider)
+  : new Web3(new Web3.providers.HttpProvider(defaultNetwork.rpc));
 
 export default web3;
