30e52aafe541d9d85fe608aa116287d078da7114
KJlmfe <kjlmfe@gmail.com>
2018-03-11 12:15:58 +0800

    feat(homeview): change HomeView content && layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,75 +1,43 @@
 <template>
   <main class="HomeView">
-
-    <section class="hero is-primary">
+    <section class="hero is-primary is-fullheight">
       <div class="hero-body">
-        <div class="container">
-          <h1 class="title">
-            {{$t('HomeView.hero.title')}}
-          </h1>
-          <h2 class="subtitle">
+        <div class="container has-text-centered">
+          <div class="animated bounceInDown box">
+            <h1 class="title">
+              <img src="../assets/heroLogo.png"
+                   :alt="$t('HomeView.hero.title')" />
+            </h1>
+          </div>
+
+          <h2 class="animated fadeIn subtitle home-hero">
             {{$t('HomeView.hero.subtitle')}}
           </h2>
+
+          <router-link :to="{ name: 'ExploreView'}"
+                       class="animated bounceInUp is-outlined is-white button is-medium">
+            {{$t('HomeView.hero.explore')}}
+          </router-link>
         </div>
       </div>
     </section>
 
-    <div v-if="false"
-         class="searchContent">
-      <div class="searchInput">
-        <input class="searchValue"
-               type="search"
-               placeholder="输入Dap名称或玩家名" />
-        <button class="searchButton"><img src="../../static/images/front/search.jpg"
-               alt=""></button>
-      </div>
-      <div class="searchHot">
-        <p>热门搜索：
-          <span>Cryptoitties</span>
-          <span>ForkDelta</span>
-          <span>ETHERBOTS</span>
-        </p>
-      </div>
-    </div>
-
-    <b-tabs class="box"
-            v-model="activeTab">
-      <b-tab-item :label="$t('HomeView.appList.tabTitle')">
-        <GameRank :item="item"></GameRank>
-      </b-tab-item>
-
-      <b-tab-item :label="$t('HomeView.userList.tabTitle')">
-        <PlayerRank></PlayerRank>
-      </b-tab-item>
-    </b-tabs>
   </main>
 </template>
 
 <script>
-import GameRank from '@/components/HomeView/GameRank';
-import PlayerRank from '@/components/HomeView/PlayerRank';
-import { getPlayerRank } from '@/api';
-
 export default {
   name: 'HomeView',
 
-  components: {
-    GameRank,
-    PlayerRank,
-  },
+  components: {},
 
   data() {
-    return {
-      activeTab: 0,
-      item: [],
-    };
+    return {};
   },
 
   computed: {},
 
-  async created() {
-    this.item = await getPlayerRank();
-  },
+  created() {},
 
   methods: {},
 
@@ -78,58 +46,25 @@ export default {
 </script>
 
 <style scoped>
+.HomeView {
+  margin-top: 0;
+}
 .hero {
   width: 100vw;
   transform: translateX(-5%);
-  margin-bottom: 2rem;
 }
-/* .hero.is-primary {
-  background-color: #00d1b2;
-} */
-.searchHot {
-  font-size: 14px;
-  margin: 0.5em 0 2em 10%;
-}
-.searchHot p span {
-  margin: 0 1em 0 0.3em;
-}
-.searchInput {
-  margin: 0.1em 10%;
-  height: 15%;
-}
-.searchButton {
-  width: 13%;
-  height: 2.9em;
-  background: #4fd2b3;
-  margin-left: -0.4em;
-  outline: none;
-  box-shadow: 0.2em 0px 50px rgba(0, 0, 0, 0.2);
-}
-.searchButton img {
-  width: 2em;
-  height: 2em;
-  vertical-align: middle;
-}
-.searchContent {
-  width: 70%;
-  margin: 2em auto;
-  border: 1px solid #ccc;
-  padding: 2em auto;
-}
-.searchValue {
-  border: 0 solid #ccc;
-  box-shadow: 0px 0px 50px rgba(0, 0, 0, 0.2);
-  margin: 3em 0 1em 0;
+.hero .box {
   display: inline-block;
-  width: 60%;
-  height: 2.8em;
-  line-height: 2.4em;
-  font-size: 16px;
-  outline: none;
-  padding-left: 1em;
-  padding-bottom: 0.3em;
+  padding: 2.5rem;
+  border-radius: 32px;
+  box-shadow: 0 20px 60px hsla(0, 0%, 4%, 0.05), 0 5px 10px hsla(0, 0%, 4%, 0.1),
+    0 1px 1px hsla(0, 0%, 4%, 0.2);
 }
-input::-webkit-input-placeholder {
-  color: #c4c4c4;
+.hero .title {
+  color: #7957d5;
+}
+.button.is-white {
+  border-color: #fff;
+  color: #fff;
 }
 </style>
